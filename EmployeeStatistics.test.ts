4e69721aea074b95aa409ef99aa8478e50774d6e
Halasskaaa <phanthilinhtra25@gmail.com>
2026-04-11 18:23:48 +0200

    Add tests for getAverageAge method

--- EmployeeStatistics.test.ts
+++ EmployeeStatistics.test.ts
@@ -40,6 +40,22 @@ describe("getMaxSalary", () => {
     });
 });
 
+describe("getAverageAge", () => {
+    test("should calculate correct average", () => {
+        expect(lista.getAverageAge()).toBe((30 + 25 + 40) / 3);
+    });
+    
+    test("different dataset", () => {
+        const stats = new EmployeeStatistics([
+            {name: "A", age: 20, salary: 1000},
+            {name: "B", age: 30, salary: 5000}
+        ]);
+        
+        expect(stats.getAverageAge()).toBe(30);
+    });
+});
+
+
 
 
 
